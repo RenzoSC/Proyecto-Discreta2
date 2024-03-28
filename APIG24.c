ae6f5df066548b759ba792c193fcec7e44434bc5
RenzoSC <renzoc009@gmail.com>
2024-03-28 20:49:52 -0300

    first try struct Grafo

--- APIG24.c
+++ APIG24.c
@@ -1,1 +1,104 @@
+
 #include "APIG24.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "EstructuraGrafo24.h"
+
+#define MAXLINE 1024
+
+Grafo construir_grafo(){
+    Grafo grafo = (Grafo)malloc(sizeof(struct Grafo_St));
+    if (grafo == NULL) return NULL;
+    
+    u32 m,n;
+    bool error = false;
+    char car[MAXLINE];
+    bool* is_already;
+    while (fgets(car, MAXLINE, stdin ))
+    {
+        if (car[0] == 'c') continue;
+
+        if (car[0]== 'e')
+        {   
+            printf("Se obtuvo un edge antes de indicar la cantidad de vértices y lados del grafo...\nSaliendo...");
+            exit(-1);
+        }else if (car[0] == 'p')
+        {
+            int ret = sscanf(car, "p edge %u %u\n",&n,&m);
+            if (ret <=2)
+            {
+                printf("Hubo un error al leer el numero de vertices y  lados %i", ret);
+                exit(-1);
+            }
+            
+            grafo->num_vertices = n;
+            grafo->num_lados = m;
+            grafo->list_vertice = malloc(sizeof(vertice)*n);
+            is_already = malloc(sizeof(bool)*n);
+            grafo->list_vecinos = malloc(sizeof(u32)*m*2);
+            break;
+        }
+    }
+
+    for (size_t i = 0; i < grafo->num_vertices; i++)
+    {
+        is_already[i]= false;     //acordar de destruir el array
+    }
+    
+    for (size_t i = 0; i < grafo->num_lados; i++)
+    {
+        char *is_eof = fgets(car, MAXLINE, stdin);
+        if (is_eof==NULL && i < grafo->num_lados-1)
+        {
+            return NULL;
+        }else if (is_eof==NULL)
+        {
+            printf("error al leer la linea con fgets....");
+            exit(-1);
+        }
+        
+        if (car[0]=='e')
+        {   
+            u32 vert1,vert2;
+            int r = sscanf(car,"e %u %u", &vert1,&vert2);
+            if (r<=2)
+            {
+                printf("No se pasaron 2 vertices como correspondería");
+                exit(-1);
+            }
+
+            if (is_already[vert1-1]==false)
+            {
+                vertice vertice1= create_vertice(vert1);
+                grafo->list_vertice[vert1-1]= vertice1;
+                is_already[vert1-1]=true;
+            }
+            
+            if (is_already[vert2-1]==false)
+            {
+                vertice vertice2= create_vertice(vert2);
+                grafo->list_vertice[vert2-1]= vertice2;
+                is_already[vert2-1]= true;
+            }
+            
+            add_grado(grafo->list_vertice[vert1-1]);
+            add_grado(grafo->list_vertice[vert2-1]);
+            grafo->list_vecinos[vert1-1]= vert2;
+            grafo->list_vecinos[vert2-1]= vert1;
+            
+            vertice v1 = grafo->list_vertice[vert1-1];
+            vertice v2 = grafo->list_vertice[vert2-1];
+            if (grafo->delta < v1->grado || grafo->delta < v2->grado)
+            {
+                grafo->delta = v1->grado <= v2->grado ? v2->grado : v1->grado;
+            }
+            
+        }else{
+            printf("Mal formato de el paso del edge");
+        }        
+    }
+    free(is_already);
+    return grafo;
+}
--- EstructuraGrafo24.h
+++ EstructuraGrafo24.h
@@ -1,17 +1,18 @@
+
 #ifndef _ESTRUCTURAGRAFO24_H
 #define _ESTRUCTURAGRAFO24_H
 
 #include "list.h"
 #include "vertice.h"
-typedef unsigned int color;
 
 typedef unsigned int u32;
 
-typedef struct GrafoSt {
+typedef struct Grafo_St {
     u32 num_vertices;
     u32 num_lados;
     u32 delta;
     vertice * list_vertice;
+    u32 *list_vecinos;
 } GrafoSt;
 
 #endif
