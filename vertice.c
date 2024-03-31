f4cd4b2df1d04b87ccb66fec2f09910fe49be62b
RenzoSC <renzoc009@gmail.com>
2024-03-31 15:49:03 -0300

    updated vertice struct with array

--- vertice.c
+++ vertice.c
@@ -1,14 +1,15 @@
 #include <stdlib.h>
 #include <assert.h>
 
-#include "vlist.h"
 #include "vertice.h"
 
 vertice create_vertice(u32 id){
     vertice v = malloc(sizeof(struct vertice_st));
-    v->vecinos = empty();
+    v->vecinos = NULL;
     v->col = 0;
     v->id = id;
+    v->last_max = 0;
+    v->grado =0;
     assert(v!=NULL);
     return v;
 }
@@ -20,7 +21,7 @@ u32 get_vertice_id(vertice v){
 
 u32 get_grado_vertice(vertice v){
     assert(v!=NULL);
-    return list_len(v->vecinos);
+    return v->grado;
 }
 
 u32 get_color_vertice(vertice v){
@@ -30,17 +31,19 @@ u32 get_color_vertice(vertice v){
 
 u32 get_first_vecino(vertice v){
     assert(v!=NULL);
-    return list_head(v->vecinos);
+    assert(v->grado>0);
+    return v->vecinos[0];
 }
 
 u32 get_last_vecino(vertice v){
     assert(v!=NULL);
-    return list_tail(v->vecinos);
+    assert(v->grado>0);
+    return v->vecinos[v->grado-1];
 }
 
 u32 get_vecino_index(vertice v, u32 index){
     assert(v!=NULL);
-    return list_index(v->vecinos, index);
+    return v->vecinos[index];
 }
 
 void set_color(vertice v, color colorv){
@@ -51,20 +54,30 @@ void set_color(vertice v, color colorv){
 
 void add_vecino(vertice v, u32 vec){
     assert(v!=NULL);
-    v->vecinos = add_elemnt(vec, v->vecinos);
+    if (v->last_max == v->grado)
+    {   
+        u32 new_max = v->grado == 0 ? 1 : 2*v->grado;
+        v->vecinos = realloc(v->vecinos, sizeof(u32)*new_max);
+        v->last_max = new_max;
+    }
+    v->vecinos[v->grado]= vec;
+    v->grado++;
 }
 
 void pop_vecino(vertice v){
     assert(v!=NULL);
-    assert(is_empty(v->vecinos));
-    v->vecinos = pop_element(v->vecinos);
+    assert(v->grado>0);
+    v->grado --;
 }
 
 vertice destroy_vertice(vertice v){
     assert(v!=NULL);
     v->col=0;
     v->id=0;
-    v->vecinos= destroy(v->vecinos);
+    v->grado =0;
+    v->last_max = 0;
+    free(v->vecinos);
+    v->vecinos = NULL;
     free(v);
     v=NULL;
     assert(v==NULL);
